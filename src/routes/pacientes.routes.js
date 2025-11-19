895456af4e41ea727cba50aa518c4eea47854e1b
Asus Ryzen 5 <esmeralda298rivera@gmail.com>
2025-11-19 09:56:48 -0600

    Nuevas rutas

--- src/routes/pacientes.routes.js
+++ src/routes/pacientes.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getPacientes, getPacienteByNombres } from "../controllers/empleados.controller.js";
+import { getPacientes, getPacienteByNombres } from "../controllers/pacientes.controller.js";
 
 const router = Router();
 
+//PACIENTES_______________________________________________________________________________________________
 router.get("/", getPacientes);
-//ruta para buscar por nombre y apellidos a clientes
+//ruta para buscar por nombre y apellidos a pácientes
 router.get("/buscar", getPacienteByNombres);
 
 export default router;
