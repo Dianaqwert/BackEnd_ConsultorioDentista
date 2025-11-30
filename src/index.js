4ee2ce8a67fb9d8b916ce657aaa160766086f777
Asus Ryzen 5 <esmeralda298rivera@gmail.com>
2025-11-30 16:41:21 -0600

    nueva ruta

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 // Importar TODAS las rutas (nombres deben coincidir con los archivos)
 import empleadosRoutes from "./routes/empleados.routes.js";
 import pacientesRoutes from "./routes/pacientes.routes.js";   // <--- plural 'pacientes'
+import tratamientosRoutes from "./routes/tratamientos.routes.js";
 // si tienes tratamientosRoutes:
 // import tratamientoRoutes from "./routes/tratamientos.routes.js";
 
@@ -14,6 +15,7 @@ app.use(express.json());
 // Usar TODAS las rutas
 app.use("/api/empleados", empleadosRoutes);
 app.use("/api/pacientes", pacientesRoutes);
+app.use("/api/tratamientosRoutes",tratamientosRoutes)
 // app.use("/api/tratamientos", tratamientoRoutes);
 
 const PORT = 3000;
--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 
 import empleadosRoutes from "./empleados.routes.js";
 import pacientesRoutes from "./pacientes.routes.js";
+import tratamientosRoutes from "./tratamientos.routes.js";
+
 
 
 const router = Router();
@@ -9,6 +11,8 @@ const router = Router();
 // prefijos de las rutas
 router.use("/empleados", empleadosRoutes);
 router.use("/pacientes", pacientesRoutes);
+router.use("/tratamientos",tratamientosRoutes);
+
 
 
 export default router;
